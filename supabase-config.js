8ec5ca51bd425bcb94f18d820ea0c98eb26c46bc
jotave <joao.v.souza193@aluno.senai.br>
2026-05-12 10:17:31 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,10 @@
 const SUPABASE_URL = 'https://epklfkzyscrcbgqjzpej.supabase.co';
+
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwa2xma3p5c2NyY2JncWp6cGVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1NDg1NzAsImV4cCI6MjA5NDEyNDU3MH0.EMmVKp1E-34mYDZ5LTl94AhuzDpgxlp261CTJAcrO4E';
 
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+console.log('✅ Supabase conectado');
